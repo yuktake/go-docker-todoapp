924d88aeacc2a983585d601afbfdf80687248639
yukitake <koshi.yukitake@gmail.com>
2025-02-04 23:53:53 +0900

    ADD: add unique tag to user's email column

--- application/domain/user/user.go
+++ application/domain/user/user.go
@@ -12,7 +12,7 @@ type User struct {
 	ID        int64  `bun:"id,pk,autoincrement" json:"id"`
 	Name      string `bun:"name,notnull" json:"name"`
 	Password  string `bun:"password,notnull" json:"password"`
-	Email     string `bun:"email,notnull" json:"email"`
+	Email     string `bun:"email,notnull,unique" json:"email"`
 	CreatedAt time.Time
 	UpdatedAt time.Time `bun:",nullzero"`
 }
